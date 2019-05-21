da48158018163f9b1e9aace5df041805199b2ac6
Daniel Feline <daniel.feline@yahoo.com>
2019-05-21 16:51:25 +0200

    e

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+20.times do
+    article = Article.new(
+      title: Faker::FunnyName.two_word_name,
+      content: Faker::Lorem.paragraph(2),
+    )
+    article.save!
+  end
